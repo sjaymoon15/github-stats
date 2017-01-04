427d6d50fff41859622a87f4140d97be46cc5c2c
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-04 17:12:33 -0500

    add mansorny style

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { fetchRepos } from '../actions/index';
 import { connect } from 'react-redux';
+import Masonry from 'react-masonry-component';
+const masonryOptions = { transitionDuration: 0 };
 
 class SearchBar extends Component {
 	constructor(props){
@@ -18,12 +20,30 @@ class SearchBar extends Component {
 		this.setState({term: ''});
 	}
 	renderRepos(repo){
-		console.log(repo);
+		console.log('repo', repo);
 		return (
-			<li key={repo.id}>{repo.full_name}</li>
+			<div className="image-element-class each-box">
+        <img src="https://unsplash.it/200/300/?random" />
+        <p>{repo.full_name}</p>
+    	</div>
 		);
 	}
 	render(){
+		if(!this.props.repos){
+			return(
+				<form onSubmit={this.onFormSubmit} className='input-group'>
+					<input 
+						placeholder='Type your Github username'
+						className='form-control'
+						value={this.state.term}
+						onChange={this.onInputChange}
+						/>
+					<span className='input-group-btn'>
+						<button type='submit' className='btn btn-secondary'>Submit</button>
+					</span>
+				</form>
+			)
+		}
 		return (
 			<div>
 				<form onSubmit={this.onFormSubmit} className='input-group'>
@@ -37,9 +57,15 @@ class SearchBar extends Component {
 						<button type='submit' className='btn btn-secondary'>Submit</button>
 					</span>
 				</form>
-				<ul className='list-group'>
-					{this.props.repos.map(this.renderRepos)}
-				</ul>
+				<Masonry
+          className={'my-gallery-class'} // default ''
+          elementType={'div'} // default 'div'
+          options={masonryOptions} // default {}
+          disableImagesLoaded={false} // default false
+          updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+      	>
+        {this.props.repos.map(this.renderRepos)}
+      	</Masonry>
 			</div>
 		);
 	}
