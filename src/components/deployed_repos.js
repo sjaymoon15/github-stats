05d7eac6e93d7964ff77c8b8e34ea52de8649a23
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-12 14:57:20 -0500

    replace bootstrap for repo-list with mansonry

--- src/components/deployed_repos.js
+++ src/components/deployed_repos.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Repo from './repo';
-
+import Masonry from 'react-masonry-component';
 
 export default class DeployedRepos extends Component{
 
@@ -16,12 +15,19 @@ export default class DeployedRepos extends Component{
 	}
 	render(){	
 		const deployedrepos = this.props.deployedrepos;
+		const masonryOptions = { transitionDuration: 0 };
 		return (
 			<div className='each-list'>
 				<h4>Deployed Repos</h4>
-				<div className='row'>
-					{deployedrepos.map(this.renderRepos)}
-				</div>
+				<Masonry
+	        className={''} // default ''
+	        elementType={'div'} // default 'div'
+	        options={masonryOptions} // default {}
+	        disableImagesLoaded={false} // default false
+	        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+	    	>
+	        {deployedrepos.map(this.renderRepos)}
+	    	</Masonry>
 			</div>
 		);
 	}
--- src/components/recent_repos.js
+++ src/components/recent_repos.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Masonry from 'react-masonry-component';
 import Repo from './repo';
 
 
@@ -15,12 +16,19 @@ export default class RecentRepos extends Component{
 	}
 	render(){	
 		const recentrepos = this.props.recentrepos;
+		const masonryOptions = { transitionDuration: 0 };
 		return (
 			<div className='each-list'>
 				<h4>Recently Updated Repos</h4>
-				<div className='row'>
-					{recentrepos.map(this.renderRepos)}
-				</div>
+				<Masonry
+	        className={''} // default ''
+	        elementType={'div'} // default 'div'
+	        options={masonryOptions} // default {}
+	        disableImagesLoaded={false} // default false
+	        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+	    	>
+	        {recentrepos.map(this.renderRepos)}
+	    	</Masonry>
 			</div>
 		);
 	}
